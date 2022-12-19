c055ba12fc08b55dea55f79d83e7356f7d01ce4b
thijsBoet <m.boet@chello.nl>
2022-12-19 16:15:44 +0100

    finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -6,6 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => {
